4659736a008155df6acc531d542cf46f42a51690
Nikolay Chernov <nchernov@gmail.com>
2020-04-19 13:17:03 +0300

    bebel + support IE11

--- yoga/src/js/index.js
+++ yoga/src/js/index.js
@@ -3,6 +3,11 @@ import timer from './timer';
 import contact from './contact';
 import slider from './slider';
 
+// require('es6-promise').polyfill();
+require('nodelist-foreach-polyfill');
+require('formdata-polyfill');
+
+
 window.addEventListener('DOMContentLoaded', () => {
     tabs();
     timer();
